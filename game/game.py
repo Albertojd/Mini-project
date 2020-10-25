900bebfcda05ccdd4eb4dd9a11e7927fffa0fac1
Albertojd <albertojd1514@gmail.com>
2020-10-25 14:22:43 +0100

    ahorcado rudimentario

---
+++ game/game.py
@@ -0,0 +1,8 @@
+import random
+
+
+words_for_play = [python, ironhack, dog, mint, earth, bottle, book, android, pen, computer, glasses, cork, bicycle, guitar, screwdriver]
+def randomword():
+    # Esta funcion nos devolvera una palabra elegida aleatoriamente.
+    chosen_word = random.choice(words)
+    return chosen_word
---
+++ game/words.py
@@ -0,0 +1,2 @@
+words = ["python", 'ironhack', 'dog', 'cat', 'mint', 'earth', 'bottle', 'book', 'android', 'pen',
+         'computer', 'glasses', 'cork', 'bicycle', 'guitar', 'screwdriver',"ball", "headphones"]
