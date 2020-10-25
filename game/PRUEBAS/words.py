1ee4b3cc91af9bc32b1837451772cd1f23cdfd95
Albertojd <albertojd1514@gmail.com>
2020-10-25 23:51:02 +0100

    hanged_finished_2.1

--- game/PRUEBAS/words.py
+++
@@ -1,3 +0,0 @@
-animals = ['dog', 'cat', 'elephant', 'bear', 'zebra', 'shark', 'spider', 'cow', "donkey", 'snake', 'tiger', 'bird', 'hamster', 'horse']
-
-python = ['loop', 'list', 'program', 'function', 'lambda', 'map', 'filter', 'string', 'tuple', 'set', 'ironhack', 'yield', 'return', 'reduce']
--- game/hangman.py
+++ game/hangman.py
@@ -1,5 +1,6 @@
 import random
 import string
+### Las librerias words, han sido creadas para contenter las listas de palabras a adivinar
 from words import animals
 from words import python
 gallows = ['''
@@ -80,12 +81,13 @@ def hiding():
     return hiding_word
 
 hide_word = hiding()
+#####################################################################################################################
 
 progress = []  ### Lista que se rellenará con espacios en blancos, para luego ir incluyendo las letras acertadas
 count_letters= len(chosen_word) ### Esto nos ayuda a pintar el número de letras que tiene la palabra oculta
-alphabet= list(string.ascii_uppercase)
+alphabet= list(string.ascii_uppercase) ### Importamos un diccionario en mayusculas para chequear si hay alguna letra repetida
 
-for i in range(len(chosen_word)): ### Este bucle rellena la lista de progreso con los espacios en blanco correspondientes
+for i in range(len(chosen_word)): ### Este bucle rellena la lista de progress con los espacios en blanco correspondientes
     progress.append("_ ")
 
 ### La función CHECKING ayudará más adelante a comproabar sí la palabra escrita coincide con la elegida
@@ -94,9 +96,10 @@ def checking():
     for char in chosen_word:
         check_list.append(char+" ")
     return check_list
-
 check_word = checking()        
 
+########################################################################################################################
+### Función que ira reproduciendo el juego
 def game():
     used_letters=[]
     mistake = 0 
@@ -120,27 +123,32 @@ def game():
     
     
         print("".join(progress)) ###Convertimos la lista, en un string, y la pintamos
-        print("The word has ",count_letters, "letters")  ### Con esto ayudamos a ver cuantas letras tiene la palabra oculta
+        print("The word has ",count_letters, "letters")  ### Con esto pintamos cuantas letras tiene la palabra oculta
         print("Used letters: ", used_letters)  ### Pinta las letras que ya has usado
-        letter= input("Choose a uppercase letter:")
+        
+### El programa nos pedirá introducir la letras, esta parte del bucle comprobara que estamos metiend solo letras mayusculas.
+### Si es un caracter valido lo añadirá a la lista used_letters
+        letter= input("Choose a uppercase letter:") 
         if letter in used_letters:
             print("You have already used this letter")
         if letter not in alphabet:
             print("This character is not allowed")
         else:
             used_letters.append(letter)
-
+            
+###Con estos condicionales, comprobamos si la letra escogida está contenida en la palabra oculta;
+### Si no es así sumará una unidad al contador de errores
             wrong = True  ##Sí la letra está mal
             for i in range(len(chosen_word)):
                 if letter == chosen_word[i]:
                     progress[i]= letter + " "
                     wrong = False
-
             if wrong:
                 mistake += 1
             if mistake == 6:
                 print("YOU LOSE, The word was: ", chosen_word.upper())
 
+###Por último comprobamos si la palabra escrita por ti mismo, coincide con la elegida por el programa; después, pinta un mensaje
             if check_word == progress:
                 print("".join(progress))
                 print("YOU WIN!, The word is: ", chosen_word.upper())
